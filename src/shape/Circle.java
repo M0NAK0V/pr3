f8090bb3ca8a490f96312db1b2f80df57c3309e9
M0NAKOV <monakow.andrew@yandex.ru>
2020-11-29 22:57:45 +0300

    Initial commit

---
+++ src/shape/Circle.java
@@ -0,0 +1,47 @@
+package shape;
+import java.math.*;
+public class Circle extends Shape{
+
+    protected double radius;
+
+    public Circle(){
+        this.radius = 4;
+        this.color = "red";
+        this.filled = false;
+    }
+
+    public Circle(double radius){
+        this.radius = radius;
+        this.color = "blue";
+        this.filled = false;
+    }
+
+    public Circle(double radius, String color, boolean filled){
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimetr() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString(){
+        return "Our shape: circle, his radius — " + this.radius +", his filled — " +this.filled+", and color — "+this.color+".";
+    }
+}
